dacd66d45751e9d8f7dff45d9883505c026d2707
lfilipe09 <luis.andrade987@gmail.com>
2023-10-22 02:36:31 -0300

    feat: setup axios service

--- src/types/product.ts
+++ src/types/product.ts
@@ -3,3 +3,28 @@ export type PriceProps = {
   amount: number
   decimals: number
 }
+
+export type ItemProps = {
+  id: string
+  title: string
+  price: {
+    currency: string
+    amount: number
+    decimals: number
+  }
+  picture_url: string
+  condition: string
+  free_shipping: boolean
+  city: string
+}
+
+export type CategoryAPIProps = {
+  name: string
+  id: string
+}
+
+export type ItemsAPIProps = {
+  query: string
+  categories: CategoryAPIProps[]
+  items: ItemProps[]
+}
